31d8d2a82635f4969eeed08f38a169f4ec7a6d0e
leeyunhome <leeyunhome@daum.net>
2021-05-05 00:39:06 +0900

    upto 8.8 메뉴만들기 예제_by_myself

--- Chapter8/Lecture5/lecture5.c
+++ Chapter8/Lecture5/lecture5.c
@@ -8,25 +8,25 @@ int main()
 	char c;
 	int rows, cols;
 
-	//while (1)
-	//{
-	//	scanf("%c %d %d", &c, &rows, &cols);
-	//	while (getchar() != '\n') continue;
-	//	display(c, rows, cols);
-	//	if (c == '\n')
-	//		break;	// doesn't work well
-	//}
-
-	printf("Input one character and two integers:\n");
-	while ((c = getchar()) != '\n')
+	while (1)
 	{
-		scanf("%d %d", &rows, &cols);
-		while (getchar() != '\n') continue;
-
+		scanf("%c %d %d", &c, &rows, &cols);
+		//while (getchar() != '\n') continue;
 		display(c, rows, cols);
-		printf("Input another character and two integers:\n");
-		printf("Press Enter to quit.\n");
+		if (c == '\n')
+			break;	// doesn't work well
 	}
+
+	//printf("Input one character and two integers:\n");
+	//while ((c = getchar()) != '\n')
+	//{
+	//	scanf("%d %d", &rows, &cols);
+	//	while (getchar() != '\n') continue;
+
+	//	display(c, rows, cols);
+	//	printf("Input another character and two integers:\n");
+	//	printf("Press Enter to quit.\n");
+	//}
 
 	return 0;
 }
--- Chapter8/Lecture8/lecture8.c
+++ Chapter8/Lecture8/lecture8.c
@@ -2,54 +2,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//char get_choice(void);
-//char get_first_char(void);
-//int  get_integer(void);
-//void count(void);
+char get_choice(void);
+char get_first_char(void);
+int  get_integer(void);
+void count(void);
 
 int main()
 {
+	char sel = 0;
 
+	printf("a. Assemble\tb. beep\n");
+	printf("c. count\t4. q to quit.\n");
 
+	sel = get_choice();
 
-
+	switch (sel)
+	{
+	case 'a':
+		printf("Avengers Assemble.\n");
+		break;
+	case 'b':
+		printf("\a");
+		break;
+	case 'c':
+		count();
+		break;
+	default:
+		break;
+	}
 	return 0;
 }
 
-//void count(void)
-//{
-//
-//
-//
-//}
-//
-//char get_choice(void)
-//{
-//	int user_input;
-//	
-//
-//
-//
-//
-//	return user_input;
-//}
-//
-//char get_first_char(void)
-//{
-//	int ch;
-//
-//
-//
-//
-//
-//	return ch;
-//}
-//
-//int get_integer(void)
-//{
-//	int input;
-//
-//
-//
-//	return input;
-//}
+void count(void)
+{
+	int input = 0;
+	int i;
+
+	scanf("%d", &input);
+
+	for (i = 0; i < input; ++i)
+	{
+		printf("%d\n", input - i);
+	}
+
+}
+
+char get_choice(void)
+{
+	int user_input;
+	
+	scanf("%d", &user_input);
+
+	return user_input;
+}
+
+char get_first_char(void)
+{
+	int ch;
+
+	scanf("%c", &ch);
+
+	while (getchar() != '\n')
+		continue;
+
+	return ch;
+}
+
+int get_integer(void)
+{
+	int input;
+
+	while (scanf("%d", &input) != 1)
+	{
+		printf("Wrong input. Please try again.\n");
+	}
+
+	return input;
+}
