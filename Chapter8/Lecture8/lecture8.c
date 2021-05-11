116bfad42f0773a77cb2811167aa81e609366cfb
leeyunhome <leeyunhome@daum.net>
2021-05-11 14:19:49 +0900

    add 10.8

--- Chapter8/Lecture8/lecture8.c
+++ Chapter8/Lecture8/lecture8.c
@@ -2,54 +2,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//char get_choice(void);
-//char get_first_char(void);
-//int  get_integer(void);
-//void count(void);
+char get_choice(void);
+char get_first_char(void);
+int  get_integer(void);
+void count(void);
 
 int main()
 {
-
-
+	printf("Select menu");
+	printf("1. input an char\t2. ")
 
 
 	return 0;
 }
 
-//void count(void)
-//{
-//
-//
-//
-//}
-//
-//char get_choice(void)
-//{
-//	int user_input;
-//	
-//
-//
-//
-//
-//	return user_input;
-//}
-//
-//char get_first_char(void)
-//{
-//	int ch;
-//
-//
-//
-//
-//
-//	return ch;
-//}
-//
-//int get_integer(void)
-//{
-//	int input;
-//
-//
-//
-//	return input;
-//}
+void count(void)
+{
+	int i;
+	int num;
+
+	scanf("%d", &num);
+
+	for (i = 0; i < num; ++i)
+	{
+		printf("%d\n", i + 1);
+	}
+}
+
+char get_choice(void)
+{
+	int user_input;
+	
+
+
+
+
+	return user_input;
+}
+
+char get_first_char(void)
+{
+	int ch;
+
+	while (ch = getchar() != '\n')
+		continue;
+
+	return ch;
+}
+
+int get_integer(void)
+{
+	int input;
+
+
+
+	return input;
+}
