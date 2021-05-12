8e85cb3b377a0dd0323630b1b2bd336a59487b85
leeyunhome <leeyunhome@daum.net>
2021-05-12 13:50:27 +0900

    add 14.11 신축성 있는 베열 멤버

--- Chapter11/10_CommandLineArguments/10_CommandLineArguments.c
+++ Chapter11/10_CommandLineArguments/10_CommandLineArguments.c
@@ -3,7 +3,16 @@
 
 int main(int argc, char * argv[])
 {
+	int count;
 
+	printf("The command line has %d argument:\n", argc);
+
+	for (count = 0; count < argc; count++)
+	{
+		printf("Arg %d : %s\n", count, argv[count]);
+		printf("argc %d\n", argc);
+	}
+	printf("\n");
 
 	return 0;
 }
