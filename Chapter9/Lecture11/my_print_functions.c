d0b66227f99719e4559feeb984ba507d57f29331
leeyunhome <leeyunhome@daum.net>
2021-05-16 08:34:15 +0900

    add 멀티쓰레딩

--- Chapter9/Lecture11/my_print_functions.c
+++ Chapter9/Lecture11/my_print_functions.c
@@ -4,18 +4,19 @@
 
 void print_hello()
 {
-
-
+	printf("Hello\n");
+	// many more lines
 }
 
 void print_hi()
 {
-
-
+	printf("Hi\n");
+	// many more lines
 }
 
 void print_str(char* str)
 {
-
-
+	printf("%s\n", str);
 }
+
+// assume that we have many more longer functions
--- Chapter9/Lecture15/lecture15.c
+++ Chapter9/Lecture15/lecture15.c
@@ -3,7 +3,30 @@
 
 int main()
 {
+	//int a = 0;
 
+	//printf("%p\n", &a);
+	//printf("%d\n", a);
+
+	//int* ptr = NULL;
+
+	//printf("%p\n", ptr);
+	//printf("%d\n", *ptr);
+
+	int* safer_ptr = NULL;
+
+	int a = 122;
+
+	//safer_ptr = &a;
+
+	if (a % 2 == 0)
+		safer_ptr = &a;
+
+	if (safer_ptr != NULL)
+		printf("%p\n", safer_ptr);
+
+	if (safer_ptr != NULL)
+		printf("%d\n", *safer_ptr);
 
 	return 0;
 }
