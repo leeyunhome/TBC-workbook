41516a8e162117a688a73aece473739c4b860e0e
leeyunhome <leeyunhome@daum.net>
2021-05-22 09:13:02 +0900

    add 함수 포인터의 배열 연습문제

--- Chapter14/24_QSortAndFunctionPointer/main.c
+++ Chapter14/24_QSortAndFunctionPointer/main.c
@@ -2,18 +2,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct kid
+{
+	char name[100];
+	int height;
+};
+
 //TODO: try increasing/decreasing order
-//int compare(const void* first, const void* second)
-//{
-//
-//}
+int compare(const void* first, const void* second)
+{
+	if (*(float*)first > *(float*)second)
+		return 1;
+	else if (*(float*)first < *(float*)second)
+		return -1;
+	else
+		return 0;
+}
 
 int main()
 {
+	//int arr[] = { 8, 2, 5, 3, 6, 11 };
 	float arr[] = { 8.1f, 2.4f, 5.6f, 3.2f, 6.1f, 11.0f };
+	int n = sizeof(arr) / sizeof(arr[0]);
 
+	qsort(arr, n, sizeof(float), compare);
 
-
+	for (int i = 0; i < n; i++)
+		printf("%f ", arr[i]);
 
 	return 0;
 }
