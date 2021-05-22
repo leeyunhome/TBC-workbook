4d689b6cd287da5782eefe1eb03e293d8147f986
leeyunhome <leeyunhome@daum.net>
2021-05-22 20:27:39 +0900

    add 2의 보수 표현법 확인해보기

--- Chapter15/01_BitwiseLogicalOperators/main.c
+++ Chapter15/01_BitwiseLogicalOperators/main.c
@@ -4,7 +4,7 @@
 #include <string.h> // strlen()
 #include <stdlib.h>	// exit()
 
-//unsigned char to_decimal(const char bi[]);
+unsigned char to_decimal(const char bi[]);
 
 int main()
 {
@@ -21,7 +21,19 @@ int main()
 	return 0;
 }
 
-//unsigned char to_decimal(const char bi[])
-//{
-//
-//}
+unsigned char to_decimal(const char bi[])
+{
+	const size_t bits = strlen(bi);
+	unsigned char sum = 0;
+	for (size_t i = 0; i < bits; ++i)
+	{
+		if (bi[i] == '1')
+			sum += (int)pow(2, (bits - 1 - i));
+		else if (bi[i] != '0') {
+			printf("Wrong character : %c", bi[i]);
+			exit(1);
+		}
+	}
+
+	return sum;
+}
