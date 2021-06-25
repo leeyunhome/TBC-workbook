756a717c6871d07422042255b6dda838f0d84fd6
leeyunhome <leeyunhome@daum.net>
2021-06-26 06:43:14 +0900

    16.5  가변 인수 매크로

--- Chapter16/05_VariadicMacros/main.c
+++ Chapter16/05_VariadicMacros/main.c
@@ -6,7 +6,7 @@
 	Variadic Macros	accept a variable number of arguments.
 */
 
-
+#define PRINT(X, ...) printf("Message " #X ": " __VA_ARGS__)
 
 // ... : ellipses
 // __VA_ARGS : one of the predefined macros
@@ -18,7 +18,15 @@ stdvar.h Variadic arguments
 
 int main()
 {
+	double x = 48;
+	double y;
 
+	y = sqrt(x);
+	PRINT(1, "x = %g\n", x);
+	printf("Message " "1" ": " "x = %g\n", x);
+
+	PRINT(2, "X = %.2F, y = %.4f\n", x, y);
+	printf("Message " "2" ": " "x = %.2f, y = %.4f\n", x, y);
 
 	return 0;
 }
