004bbe49788901952cac5d308bcb555917e0c01a
leeyunhome <leeyunhome@daum.net>
2021-06-26 05:26:05 +0900

    add 16.3 #define  매크로

--- Chapter15/05_BitMasks/main.c
+++ Chapter15/05_BitMasks/main.c
@@ -73,10 +73,34 @@ int main()
 
 void char_binary(const char num)
 {
+	printf("Decimal %3d \t== Binary ", num);
 
+	const size_t bits = sizeof(num) * 8;
+	for (size_t i = 0; i < bits; ++i)
+	{
+		const char mask = 1 << (bits - 1 - i);
+
+		if ((num & mask) == mask)
+			printf("%d", 1);
+		else
+			printf("%d", 0);
+	}
+
+	printf("\n");
 }
 
 void int_binary(const int num)
 {
+	printf("Decimal %3d \t== Binary ", num);
 
+	const size_t bits = sizeof(num) * 8;
+	for (size_t i = 0; i < bits; ++i)
+	{
+		const int mask = 1 << (bits - 1 - i);
+
+		if ((num & mask) == mask)
+			printf("%d", 1);
+		else
+			printf("%d", 0);
+	}
 }
--- Chapter16/02_Define/main.c
+++ Chapter16/02_Define/main.c
@@ -10,14 +10,34 @@
 	- An instruction that represents a sequence of instructions in abbreviated form.
 */
 
-/*
-#define			SAY_HELLO		printf("Hello, World!");
+#define PI 33.141592	/* Symbolic, or manifest, constants */
+#define GRAVITY 9.8
 
-Preprocessor	Macro (name)	body (or replacement list)
-directive
+#define THREE 3
+#define MESSAGE "The greatest glory in living lies not in nvever falling, \
+but in rising every time we fall."
 
-Macro expansion
-*/
+#define NINE THREE*THREE
+#define	SAY_HELLO for(int i = 0; i < 10; i++) printf("Hello, World!\n");
+#define FORMAT "Number is %d.\n"
+
+#define WITH_BLANK 1
+
+#define MY_CH 'Z'
+#define MY_ST "Z"	// Z\0
+
+#define LIMIT 20
+const int LIM = 50;
+static int arr1[LIMIT];
+//static int arr2[LIM];		// clang, C++
+const int LIM2 = 2 * LIMIT;
+//const int LIM3 = 2 * LIM;	// clang, C++
+//
+//Preprocessor	Macro (name)	body (or replacement list)
+//directive
+//
+//Macro expansion
+//
 
 
 /*
@@ -38,12 +58,21 @@ Macro expansion
 */
 
 /* Redefining Constants */
-
-
+#define SIX 2*3
+#define SIX 2*3
+#undef SIX
+#define SIX 2 * 3	// Warning
 
 int main()
 {
+	int n = THREE;
 
+	SAY_HELLO;	//Note the additional ;
+	n = NINE;
+
+	printf(FORMAT, n);
+	printf("%s\n", MESSAGE);	// replaced
+	printf("SAY_HELLO NINE\n");	// Not replaced
 
 	return 0;
 }
