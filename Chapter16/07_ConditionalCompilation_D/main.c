7cc8f91a84a062bf516890120ac5ce0dcefe07e3
leeyunhome <leeyunhome@daum.net>
2021-07-10 17:34:50 +0900

    add up to 16.7 조건에 따라 다르게 컴파일하기

--- Chapter16/07_ConditionalCompilation_D/main.c
+++ Chapter16/07_ConditionalCompilation_D/main.c
@@ -1,11 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+//#define REPORT
+//#undef REPORT
 
+int sum(int i, int j)
+{
+	int s = 0;
+	for (int k = i; k <= j; ++k)
+	{
+		s += k;
+
+#ifdef _DEBUG
+		printf("%d %d\n", s, k);
+#endif // REPORT
+	}
+
+	return s;
+}
 
 int main()
 {
-
+	printf("\n%d \n", sum(1, 10));
 
 	return 0;
 }
--- Chapter16/07_OtherDirectives/main.c
+++ Chapter16/07_OtherDirectives/main.c
@@ -9,11 +9,14 @@
 	#undef
 */
 
+#define LIMIT 400
+//#undef LIMIT	// It's ok to undefine previously NOT defined macro.
 
+#undef NON_DEFINED
 
 int main()
 {
-
+	printf("%d\n", LIMIT);
 	   
 	return 0;
 }
