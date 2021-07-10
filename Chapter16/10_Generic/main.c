312b0d7911f2f43fca42ec5a7c48b0e5a224a0ba
leeyunhome <leeyunhome@daum.net>
2021-07-10 23:18:31 +0900

    16.10 _Generic 표현식

--- Chapter16/10_Generic/main.c
+++ Chapter16/10_Generic/main.c
@@ -2,20 +2,46 @@
 #include <stdio.h>
 
 /*
-	Generic selection expression
-	- generic programming : code is not specific to a particular type
+    Generic selection expression
+    - generic programming : code is not specific to a particular type
 
-	_Generic : C11 keyword
+    _Generic : C11 keyword
 */
 
+// #define MYTYPE(X) _Generic((X), \
+//     int: "int", \
+//     float: "float", \
+//     double: "double", \
+//     default: "other"\
+// )
 
+// #define MYTYPE(X) _Generic((X), \
+//     int: "int", \
+//     float: "float", \
+//     double: "double", \
+//     long: "long", \
+//     int* : "int*", \
+//     default: "other"\
+// )
+
+#define MYTYPE(X) _Generic((X), \
+    int: (X + 123), \
+    float: "float", \
+    double: "double", \
+    long: "long", \
+    int* : "int*", \
+    default: "other"\
+)
 
 int main()
 {
-	int d = 5;
-	
+    int d = 5;
+
+    printf("%d\n", MYTYPE(d));
+    printf("%s\n", MYTYPE(2.0 * d));
+    printf("%s\n", MYTYPE(3L));
+    printf("%s\n", MYTYPE(&d));
 
 
-
-	return 0;
+    return 0;
 }
