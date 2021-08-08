645ba6e7086e8f64a940f36c56bc812cbe22d6eb
leeyunhome <leeyunhome@daum.net>
2021-08-08 20:23:04 +0900

    12.14 메모리 누수와 free()의 중요성

--- Chapter12/14_Free/Free.c
+++ Chapter12/14_Free/Free.c
@@ -5,7 +5,7 @@
 int main()
 {
 	printf("Dummy Output\n");
-	//int* ptr_backup = NULL;
+	int* ptr_backup = NULL;
 
 	for (int k = 0; k < 1000000; ++k)
 	{
@@ -24,16 +24,16 @@ int main()
 
 		printf("%d %d\n", ptr[0], ptr[1]);
 
-		// 주소를 옮겨적어놨다면
+		//// 주소를 옮겨적어놨다면
 		//ptr_backup = ptr;
 
-		free(ptr);
+		//free(ptr);
 		//ptr = NULL;
 	}
 
 	// what happens ?	
 	//printf("%d %d\n", ptr_backup[0], ptr_backup[1]);
-	//printf("Dummy Output\n");
+	printf("Dummy Output\n");
 
 	return 0;
 }
