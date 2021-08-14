fe667395812c3687ccd4886a0e0ceebe8127c587
leeyunhome <leeyunhome@daum.net>
2021-08-14 16:51:03 +0900

    14.10 복합 리터럴

--- Chapter14/10_CompoundLiterals/main.c
+++ Chapter14/10_CompoundLiterals/main.c
@@ -19,15 +19,15 @@ struct rectangle
 	double height;
 };
 
-//double rect_area(struct rectangle r)
-//{
-//
-//}
-//
-//double rect_area_ptr(struct rectangle * r)
-//{
-//
-//}
+double rect_area(struct rectangle r)
+{
+	return r.width * r.height;
+}
+
+double rect_area_ptr(struct rectangle * r)
+{
+	return r->width * r->height;
+}
 
 int main()
 {
@@ -41,8 +41,23 @@ int main()
 
 	//book_to_read = { "Alice in Wonderland", "Lewis Carroll", 20.3f };//Error
 
+	strcpy(book_to_read.title, "Alice in Wonderland");
+	strcpy(book_to_read.author, "Lewis Carroll");
+	book_to_read.price = 20.3f;
 
+	struct book book2 = { "Alice in Wonderland" , "Lewis Carroll", 20.3f };
+	book_to_read = book2;
 
+	book_to_read = (struct book){ "Alice in Wonderland" , "Lewis Carroll", 20.3f };
+
+	printf("%s %s\n", book_to_read.title, book_to_read.author);
+
+	struct rectangle rec1 = { 1.0, 2.0 };
+	double area = rect_area(rec1);
+	area = rect_area((struct rectangle) { 1.0, 2.0 });
+	
+	area = rect_area_ptr(&(struct rectangle) { .height = 3.0, .width = 2.0 });
+	printf("%f\n", area);
 
 	return 0;
 }
