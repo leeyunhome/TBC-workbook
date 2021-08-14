c1704d08fe0f6517fc842528aa19b50ee49135ef
leeyunhome <leeyunhome@daum.net>
2021-08-14 13:22:55 +0900

    14.6 구조체와 포인터

--- Chapter14/06_PointersToStructures/main.c
+++ Chapter14/06_PointersToStructures/main.c
@@ -14,12 +14,23 @@ struct friend {
 
 int main(void)
 {
+	struct friend my_friend[2] = {
+		{{"kaede", "matsushima"}, "1234-1234"},
+		{{"Taylor", "Swift"}, "6550-8888"}
+	};
 
+	struct friend* girl_friend;
 
+	girl_friend = &my_friend[0];
 
+	printf("%zd\n", sizeof(struct friend));
+	printf("%lld %s\n", (long long)girl_friend, girl_friend->full_name.given);
 
+	girl_friend++;
 
+	printf("%lld %s\n", (long long)girl_friend, (*girl_friend).full_name.given);
 	// . has higher precedence than *
+
 
 	return 0;
 }
