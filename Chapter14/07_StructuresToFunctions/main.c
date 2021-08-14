ac3479ad347e99fd210809f4df11b99197bf514a
leeyunhome <leeyunhome@daum.net>
2021-08-14 14:30:16 +0900

    14.7 구조체를 함수로 전달하는 방법

--- Chapter14/07_StructuresToFunctions/main.c
+++ Chapter14/07_StructuresToFunctions/main.c
@@ -9,7 +9,7 @@ struct fortune {
 	char fund_name[FUNDLEN];
 	double fund_invest;
 
-	//float* arr;
+	//float* arr; // 주소를 복사해요
 };
 
 double sum(const struct fortune *my_fortune);
@@ -31,5 +31,5 @@ int main()
 
 double sum(const struct fortune *mf)	//TOOD: try pointers
 {
-	return mf->bank_saving + mf->fund_invest;
+	return mf->bank_saving + mf->fund_invest; // 포인터기 때문에 -> 오퍼레이터
 }
