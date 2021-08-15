563c86ff523212502137ec77cb644abd2d942078
leeyunhome <leeyunhome@daum.net>
2021-08-15 21:45:42 +0900

    14.25 함수 포인터의 용법을 조금 더 자세하게 공부하기 위해서 qsort함수 예제를 풀어봄

--- Chapter14/24_QSortAndFunctionPointer/main.c
+++ Chapter14/24_QSortAndFunctionPointer/main.c
@@ -9,26 +9,43 @@ struct kid
 };
 
 //TODO: try increasing/decreasing order
-int compare(const void* first, const void* second)
-{
-	if (*(float*)first > *(float*)second)
-		return 1;
-	else if (*(float*)first < *(float*)second)
-		return -1;
-	else
-		return 0;
-}
+//int compare(const void* first, const void* second)
+//{
+//	if (*(float*)first > *(float*)second)
+//		return 1;
+//	else if (*(float*)first < *(float*)second)
+//		return -1;
+//	else
+//		return 0;
+//}
+int compare(const void* first, const void* second);
 
 int main()
 {
 	//int arr[] = { 8, 2, 5, 3, 6, 11 };
-	float arr[] = { 8.1f, 2.4f, 5.6f, 3.2f, 6.1f, 11.0f };
-	int n = sizeof(arr) / sizeof(arr[0]);
+	//float arr[] = { 8.1f, 2.4f, 5.6f, 3.2f, 6.1f, 11.0f };
+	//int n = sizeof(arr) / sizeof(arr[0]);
 
-	qsort(arr, n, sizeof(float), compare);
+	struct kid my_friends[] = {
+		"Jack Jack", 40, "Geenie", 300, "Unicorn", 170, "Piona", 150
+	};
+
+	const int n = sizeof(my_friends) / sizeof(struct kid);
+
+	qsort(my_friends, n, sizeof(struct kid), compare);
 
 	for (int i = 0; i < n; i++)
-		printf("%f ", arr[i]);
+		printf("%s	\t%d\n", my_friends[i].name, my_friends[i].height);
 
 	return 0;
+}
+
+int compare(const void* first, const void* second)
+{
+	if ((*(struct kid*)(first)).height > (*(struct kid*)(second)).height)
+		return 1;
+	else if ((*(struct kid*)(first)).height < (*(struct kid*)(second)).height)
+		return -1;
+	else
+		return 0;
 }
