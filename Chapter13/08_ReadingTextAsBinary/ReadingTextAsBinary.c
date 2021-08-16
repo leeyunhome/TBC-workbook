a00aa12fa2eec782dfa5b86c741e0dbc8f84dd16
leeyunhome <leeyunhome@daum.net>
2021-08-16 20:31:13 +0900

    15.2 이진수를 십진수로 바꾸기 연습문제

--- Chapter13/08_ReadingTextAsBinary/ReadingTextAsBinary.c
+++ Chapter13/08_ReadingTextAsBinary/ReadingTextAsBinary.c
@@ -6,10 +6,14 @@ int main()
 {
 	FILE* fp = fopen("test.txt", "rb");
 
+	unsigned char ch;
 
+	SetConsoleOutputCP(CP_UTF8);// UTF-8 mode
 
-
-
+	while (fread(&ch, sizeof(unsigned char), 1, fp) > 0)
+	{
+		printf("%hhu %c\n", ch, ch);
+	}
 
 	fclose(fp);
 
--- Chapter15/01_BitwiseLogicalOperators/main.c
+++ Chapter15/01_BitwiseLogicalOperators/main.c
@@ -8,32 +8,51 @@ unsigned char to_decimal(const char bi[]);
 
 int main()
 {
-	
+	printf("Binary (string) to Decimal conversion\n");
 
-
+	printf("%d\n", to_decimal("00000110"));
+	printf("%d\n", to_decimal("00010110"));
 
 	// Note: ^ (caret) means power in math.
 	// Hint: (int)pow(2, 3) == 8
 
-	
-
+	printf("%d\n", to_decimal("10010100"));
 
 	return 0;
 }
 
 unsigned char to_decimal(const char bi[])
 {
+	unsigned char result = 0;
 	const size_t bits = strlen(bi);
-	unsigned char sum = 0;
-	for (size_t i = 0; i < bits; ++i)
+	for (size_t idx = 0; idx < bits; ++idx)
 	{
-		if (bi[i] == '1')
-			sum += (int)pow(2, (bits - 1 - i));
-		else if (bi[i] != '0') {
+		if (bi[idx] == '1')
+			result += (unsigned char)pow((double)2, (double)(bits - idx - 1));
+		else if (bi[idx] != '0')
+		{
 			printf("Wrong character : %c", bi[i]);
 			exit(1);
 		}
+			
 	}
 
-	return sum;
+	return result;
 }
+//
+//unsigned char to_decimal(const char bi[])
+//{
+//	const size_t bits = strlen(bi);
+//	unsigned char sum = 0;
+//	for (size_t i = 0; i < bits; ++i)
+//	{
+//		if (bi[i] == '1')
+//			sum += (int)pow(2, (bits - 1 - i));
+//		else if (bi[i] != '0') {
+//			printf("Wrong character : %c", bi[i]);
+//			exit(1);
+//		}
+//	}
+//
+//	return sum;
+//}
