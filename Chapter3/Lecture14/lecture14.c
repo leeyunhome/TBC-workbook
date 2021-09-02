f5b0c40a3d3d8a651e1fd1b893df5f7d8567c7c0
leeyunhome <leeyunhome@daum.net>
2021-09-02 23:12:05 +0900

    3.14 복소수형

--- Chapter3/Lecture14/lecture14.c
+++ Chapter3/Lecture14/lecture14.c
@@ -3,7 +3,12 @@
 
 int main()
 {
+	// double _Imaginary i = 3;
+	/*double _Complex z = 1 + 2 * I;
 
+	z = 1 / z;
+
+	printf("1 / (1.0 + 2.0i) = %.1f%+.1fi\n", creal(z), cimage(z));*/
 	
 	return 0;
 }
