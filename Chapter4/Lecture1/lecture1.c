f1a65806fa536505efb7976ae79ae0c633e2b0a5
leeyunhome <leeyunhome@daum.net>
2021-09-02 23:28:41 +0900

    배열이라든지 scanf를 사용할 때 주의할 점 4.1 문자열 입출력하기

--- Chapter4/Lecture1/lecture1.c
+++ Chapter4/Lecture1/lecture1.c
@@ -1,14 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define MAX 40
 
 #include <stdio.h>
 
 int main()
 {
-
+	char fruit_name[MAX]; // stores only one character.
 
 	printf("What is your farovate fruit?\n");
 
+	int flag = scanf("%s", fruit_name);	// be careful with &
 
+	printf("You like %s!\n", fruit_name);
 
 	return 0;
 }
