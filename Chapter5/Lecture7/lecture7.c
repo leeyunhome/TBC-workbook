8f34c4c1a0fe1439b11feefe0b5b734330b962f9
leeyunhome <leeyunhome@daum.net>
2021-09-06 23:25:40 +0900

    5.7 나머지 연산자

--- Chapter5/Lecture7/lecture7.c
+++ Chapter5/Lecture7/lecture7.c
@@ -1,14 +1,43 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#define SEC_PER_HOURS 3600
+#define MIN_PER_HOURS 60
 
 int main()
 {
-	//int seconds = 0, minutes = 0, hours = 0;
+	int seconds = 0, minutes = 0, hours = 0;
+
+	printf("Input seconds : ");
+	scanf("%d", &seconds);
 
 
 
+	while (seconds >= 0)
+	{
 
-	//printf("Good bye\n");
+		//minutes = seconds / 60;
+		//seconds %= 60;
+
+		//hours = minutes / 60;
+		//minutes %= 60;
+
+		{
+			hours = seconds / SEC_PER_HOURS;
+			minutes = (seconds % SEC_PER_HOURS) / MIN_PER_HOURS;
+			seconds = seconds % SEC_PER_HOURS % MIN_PER_HOURS;
+		}
+	
+		// print result
+		printf("%d hours, %d minutes, %d seconds\n", hours, minutes, seconds);
+
+		printf("Input seconds : ");
+		scanf("%d", &seconds);
+	}
+	
+
+
+
+	printf("Good bye\n");
 
 	//int div, mod;
 	
