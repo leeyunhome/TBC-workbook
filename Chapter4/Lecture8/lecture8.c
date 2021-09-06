941a6b147dc7643e592ff68fae4e81f3f69b8ace
leeyunhome <leeyunhome@daum.net>
2021-09-06 22:36:21 +0900

    5.4 곱하기 연산자

--- Chapter4/Lecture8/lecture8.c
+++ Chapter4/Lecture8/lecture8.c
@@ -4,6 +4,25 @@
 int main()
 {
 	printf("%10i\n", 1234567);
+	printf("%-10i\n", 1234567);	// - flag
+	printf("%+i %+i\n", 123, -123);
+	printf("% i \n% i\n", 123, -123);
+	printf("%X\n", 17);
+	printf("%#X\n", 17);
+	printf("%05i\n", 123);
+	printf("%*i\n", 7, 456);
+
+	printf("\nPrecision\n");
+	printf("%.3d\n", 1024);
+	printf("%.5d\n", 1024);
+	printf("%.3f\n", 123456.1234567);
+	printf("%.3f\n", 123456.1235);
+	printf("%10.3f\n", 123.45678);
+	//printf("%10i\n", 1234567);
+	//printf("%10i\n", 1234567);
+	//printf("%10i\n", 1234567);
+	//printf("%10i\n", 1234567);
+	//printf("%10i\n", 1234567);
 
 
 
--- Chapter5/Lecture4/lecture4.c
+++ Chapter5/Lecture4/lecture4.c
@@ -3,17 +3,29 @@
 
 int main()
 {
-
+	double seed_money, target_money, annual_interest;
 
 	printf("Input seed money : ");
-	
+	scanf("%lf", &seed_money);
 
 	printf("Input target money : ");
-	
+	scanf("%lf", &target_money);
 
 	printf("Input annual interest (%%) : ");
-	
+	scanf("%lf", &annual_interest);
 
+	double fund = seed_money;
+	int year_count = 0;
+
+	while (fund < target_money)
+	{
+		fund = fund + (fund * annual_interest / 100.0);
+		year_count += 1;
+		//year_count++;
+		printf("%f\n", fund);
+	}
+
+	printf("It takes %d years\n", year_count);
 
 	return 0;
 }
