1c293f8c38f8eff726c8ea75fa98b5c2cc07d046
leeyunhome <leeyunhome@daum.net>
2021-10-12 21:51:59 +0900

    by myself

--- Chapter9/Lecture1_start/lecture1_start.c
+++ Chapter9/Lecture1_start/lecture1_start.c
@@ -2,16 +2,61 @@
 #include <stdio.h>
 #include <string.h>	// strlen()
 
-#define WIDTH	20
-#define NAME	"Your name"
-#define ADDRESS "City, Citizenship"
+#define WIDTH	50
+#define NAME	"Yun-Ho Lee"
+#define ADDRESS "Seoul, Korea"
+
+void print_char(char ch, int width);
+void print_name(char* name);
+void print_addr(char* addr);
 
 int main()
 {
-	printf("********************\n");
-	printf("           Your name\n");
-	printf("   City, Citizenship\n");
-	printf("********************\n");
+	print_char('*', WIDTH);
+	print_char(' ', WIDTH);
+	print_name(NAME);
+	print_char(' ', WIDTH);
+	printf("\n");
+	print_char(' ', WIDTH);
+	print_addr(ADDRESS);
+	print_char(' ', WIDTH);
+	printf("\n");
+	print_char('*', WIDTH);
+	//printf("********************\n");
+	//printf("           Your name\n");
+	//printf("   City, Citizenship\n");
+	//printf("********************\n");
 
 	return 0;
+}
+
+void print_char(char ch, int width)
+{
+	int r;
+
+	if ('*' == ch)
+	{
+		for (r = 0; r < width; r++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	else if (' ' == ch)
+	{
+		for (r = 0; r < (width - strlen(NAME)) / 2; r++)
+		{
+			printf(" ");
+		}
+	}
+}
+
+void print_name(char* name)
+{
+	printf("%s", name);
+}
+
+void print_addr(char* addr)
+{
+	printf("%s", addr);
 }
