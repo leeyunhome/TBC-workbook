1ecae439ba0daefe594c918d69fb128dc07c41b3
leeyunhome <leeyunhome@daum.net>
2021-10-31 19:47:40 +0900

    10.3 포인터의 산술 연산

--- Chapter10/Lecture3/lecture3.c
+++ Chapter10/Lecture3/lecture3.c
@@ -3,8 +3,28 @@
 
 int main()
 {
+	int* ptr = /*(int*)*/0; // try double*, long long*, char*, void*
 
+	//printf("%d", *ptr);	// Error in this lecture
 
+	printf("%p %lld\n", ptr, (long long)ptr);
+
+	//ptr++;	// /try -=, ++, --, -, +
+	ptr -= 2;
+
+	printf("%p %lld\n", ptr, (long long)ptr);
+
+	//ptr = -ptr;	// Not working
+	//ptr = +ptr;	// Not working
+
+	/* Subtraction */
+	double arr[10];
+	double* ptr1 = &arr[1], * ptr2 = &arr[3];
+
+	// ptr2 = ptr1 + ptr2;		// Not working
+	int i = ptr2 - ptr1;		// meaning?
+
+	printf("%lld %lld %d\n", (long long)ptr1, (long long)ptr2, i);
 
 	return 0;
 }
