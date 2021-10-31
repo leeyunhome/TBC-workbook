7c9fa686333a04651306117b4cc733b0a30daa67
leeyunhome <leeyunhome@daum.net>
2021-10-31 14:26:30 +0900

    10.17 변수로 길이를 정할 수 있는 배열

--- Chapter10/Lecture17/lecture17.c
+++ Chapter10/Lecture17/lecture17.c
@@ -12,18 +12,38 @@
 //{
 //}
 //
-//int sum2d_4(int row, int col, int ar[row][col])
-//{
-//}
+int sum2d_4(int row, int col, int ar[row][col])
+{
+    int r, c, tot = 0;
+    for (r = 0; r < row; r++)
+        for (c = 0; c < col; c++)
+            tot += ar[r][c];
+    return tot;
+}
 
 int main()
 {
-	int n;
+    int n;
 
-	printf("Input array length : ");
-	scanf("%d", &n);
+    printf("Input array length : ");
+    scanf("%d", &n);
 
-	//float my_arr[n];//Note: cannot change length after declaration
+    // int n = 7;
+    float my_arr[n];    // Note: cannot change length after declaration
+
+    for (int r = 0; r < n; r++)
+        my_arr[r] = (float)r;
+
+    for (int r = 0; r < n; r++)
+        printf("%f\n", my_arr[r]);
+
+    int data[ROWS][COLS] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 0, 1, 2}
+    };
+
+    printf("%d\n", sum2d_4(ROWS, COLS, data));
 
 	return 0;
 }
