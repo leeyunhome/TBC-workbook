b935fa1cedb6a88274ffc125d6439f1c86178ee0
leeyunhome <leeyunhome@daum.net>
2021-11-20 12:46:41 +0900

    3.9 고정 너비 정수

--- Chapter11/06_StringFunctions/06_StringFunctions.c
+++ Chapter11/06_StringFunctions/06_StringFunctions.c
@@ -7,79 +7,79 @@ void fit_str(char*, unsigned int);
 
 int main()
 {
-	/*
+	
 		strlen() : returns length of the string
-	*/
+	
 
-	//TODO: use debugger
-	//char msg[] = "Just,"" do it!";
-	//puts(msg);
-	//printf("Length %d\n", strlen(msg));
-	//fit_str(msg, 13);
-	//puts(msg);
-	//printf("Length %d\n", strlen(msg));
+	TODO: use debugger
+	char msg[] = "Just,"" do it!";
+	puts(msg);
+	printf("Length %d\n", strlen(msg));
+	fit_str(msg, 13);
+	puts(msg);
+	printf("Length %d\n", strlen(msg));
 
-	//TODO: implement fit_str() function
+	TODO: implement fit_str() function
 
-	/*
+	
 		strcat() and strncat() : string concatenation
-	*/
+	
 
 	char str1[100] = "First string";
 	char str2[] = "Second string";
 
-	/*strcat(str1, ", ");
-	strcat(str1, str2);*/
-	//strncat(str1, str2, 2);	//Append 2 characters
-	//puts(str1);
+	strcat(str1, ", ");
+	strcat(str1, str2);
+	strncat(str1, str2, 2);	//Append 2 characters
+	puts(str1);
 
-	//my_strcat(str1, str2);
-	//puts(str1);
-	// 
-	//TODO: implement my_strcat() function!
+	my_strcat(str1, str2);
+	puts(str1);
+	 
+	TODO: implement my_strcat() function!
 
-	/*
+	
 		strcmp() and strncmp() : compare strings (not characters)
-	*/
+	
 
-	//printf("%d\n", strcmp("A", "A"));
-	//printf("%d\n", strcmp("A", "B"));
-	//printf("%d\n", strcmp("B", "A"));
-	//printf("%d\n", strcmp("Hello", "Hello"));
-	//printf("%d\n", strcmp("Banana", "Bananas"));
-	//printf("%d\n", strcmp("Bananas", "Banana"));
-	//printf("%d\n", strncmp("Bananas", "Banana", 6));
-	//TODO: implement strcmp!
+	printf("%d\n", strcmp("A", "A"));
+	printf("%d\n", strcmp("A", "B"));
+	printf("%d\n", strcmp("B", "A"));
+	printf("%d\n", strcmp("Hello", "Hello"));
+	printf("%d\n", strcmp("Banana", "Bananas"));
+	printf("%d\n", strcmp("Bananas", "Banana"));
+	printf("%d\n", strncmp("Bananas", "Banana", 6));
+	TODO: implement strcmp!
 
-	/*
+	
 		strcpy() and strncpy()
-	*/
+	
 
-	//char dest[100] = "";	// make sure memory is enough
-	//char source[] = "Start programming!";
-	//dest = source;			// Error
-	//dest = "Start something";	// Error
-	//strcpy(dest, source);
-	//strncpy(dest, source, 5);	// '\0' is NOT added
-	//strcpy(dest, source + 6);
-	//strcpy(dest, source);
-	//strcpy(dest + 6, "coding!");
-	//puts(dest);
+	char dest[100] = "";	// make sure memory is enough
+	char source[] = "Start programming!";
+	dest = source;			// Error
+	dest = "start something";	// error
+	strcpy(dest, source);
+	strncpy(dest, source, 5);	// '\0' is not added
+	strcpy(dest, source + 6);
+	strcpy(dest, source);
+	strcpy(dest + 6, "coding!");
+	puts(dest);
 
-	/*
+	
 		sprintf()
-	*/
+	
 
-	//char str[100] = "";
-	//int i = 123;
-	//double d = 3.14;
-	//sprintf(str, "%05d.png %f", i, d);
-	//puts(str);
-	//// capture00000.png, capture00001.png, ...
+	char str[100] = "";
+	int i = 123;
+	double d = 3.14;
+	sprintf(str, "%05d.png %f", i, d);
+	puts(str);
+	 capture00000.png, capture00001.png, ...
 
-	/*
+	
 		There are more functions...		
-	*/
+	
 
 	printf("%s\n", strchr("Hello, World", 'W'));
 	printf("%s\n", strpbrk("Hello, World", "ABCDE"));
@@ -107,15 +107,15 @@ void fit_str(char* str, unsigned int size)
 
 char* my_strcat(char* destination, const char* source)
 {
-	// make ptr point to the end of destination string
+	 make ptr point to the end of destination string
 	char* ptr = destination + strlen(destination);
 
 	while (*source != '\0')
 		*ptr++ = *source++;
 
-	// null terminate destination string
+	 null terminate destination string
 	*ptr = '\0';
 
-	// destination is returned by standard strcat()
+	 destination is returned by standard strcat()
 	return destination;
 }
