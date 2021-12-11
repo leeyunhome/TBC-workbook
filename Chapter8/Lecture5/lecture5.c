414ba73e429acd335f15551c1edbe7b75d5089dc
leeyunhome <leeyunhome@daum.net>
2021-12-11 22:57:40 +0900

    Lecture6 입력확인하기

--- Chapter8/Lecture5/lecture5.c
+++ Chapter8/Lecture5/lecture5.c
@@ -8,37 +8,49 @@ int main()
 	char c;
 	int rows, cols;
 
-	while (1)
-	{
-		scanf("%c %d %d", &c, &rows, &cols);
-		//while (getchar() != '\n') continue;
-		display(c, rows, cols);
-		if (c == '\n')
-			break;	// doesn't work well
-	}
-
-	//printf("Input one character and two integers:\n");
-	//while ((c = getchar()) != '\n')
+	//while (1)
 	//{
-	//	scanf("%d %d", &rows, &cols);
+	//	scanf("%c %d %d", &c, &rows, &cols);
 	//	while (getchar() != '\n') continue;
-
 	//	display(c, rows, cols);
-	//	printf("Input another character and two integers:\n");
-	//	printf("Press Enter to quit.\n");
+	//	if (c == '\n')
+	//		break;	// doesn't work well
 	//}
+
+	printf("Input one character and two integers:\n");
+	while ((c = getchar()) != '\n')
+	{
+		scanf("%d %d", &rows, &cols);
+		while (getchar() != '\n') continue;
+
+		display(c, rows, cols);
+		printf("Input another character and two integers:\n");
+		printf("Press Enter to quit.\n");
+	}
 
 	return 0;
 }
 
 void display(char cr, int lines, int width)
 {
-	int row, col;
-
-	for (row = 0; row < lines; ++row)
+	int r, c;
+	for (r = 0; r < lines; r++)
 	{
-		for (col = 0; col < width; ++col)
+		for (c = 0; c < width; c++)
+		{
 			printf("%c", cr);
+		}
 		printf("\n");
 	}
 }
+//void display(char cr, int lines, int width)
+//{
+//	int row, col;
+//
+//	for (row = 0; row < lines; ++row)
+//	{
+//		for (col = 0; col < width; ++col)
+//			printf("%c", cr);
+//		printf("\n");
+//	}
+//}
