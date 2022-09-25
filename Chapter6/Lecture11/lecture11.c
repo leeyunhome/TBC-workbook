3893f3d34ecac56a3696542f140b1dc77e36a1d2
leeyunhome <leeyunhome@daum.net>
2022-09-25 20:10:57 +0900

    6.12 제논의 역설

--- Chapter6/Lecture11/lecture11.c
+++ Chapter6/Lecture11/lecture11.c
@@ -3,9 +3,8 @@
 
 int main()
 {
-	/*for (int n = 1, nsqr = n * n; n < 10; n++, nsqr = n * n)
-		printf("%d %d\n", n, nsqr);*/
-
+	for (int n = 1, nsqr = n * n; n < 10; n++, nsqr = n * n)
+		printf("%d %d\n", n, nsqr);
 
 	//Note that commas in printf() are not comma operators 
 	//but separators
@@ -15,13 +14,13 @@ int main()
 	//i++, j = i;	// comma is a sequence point
 	//printf("%d %d\n", i, j);
 
-	/*int x, y, z;
+	int x, y, z;
 	z = x = 1, y = 2;
 	printf("x=%d, y=%d, z=%d\n", x, y, z);
 	z = (x = 1), (y = 2);
 	printf("x=%d, y=%d, z=%d\n", x, y, z);
 	z = ((x = 1), (y = 2));
-	printf("x=%d, y=%d, z=%d\n", x, y, z);*/
+	printf("x=%d, y=%d, z=%d\n", x, y, z);
 
 	//int my_money = 123,456; // error
 	int my_money = (123, 456);
--- Chapter6/Lecture12/lecture12_1.c
+++ Chapter6/Lecture12/lecture12_1.c
@@ -16,47 +16,99 @@ int main()
 		- ... ...
 	*/
 
+	/*
+	Zeno's Paradox
+
+	- Step 1 : 1.0 * 1.0 = 1.0, 1.0
+	- Step 2 : 1.0 * 0.5 = 0.5, 1.0 + 0.5 = 1.5
+	- Step 3 : 1.0 * 0.25 = 0.25, 1.5 + 0.25 = 1.75
+	... ...
+	*/
+
+	//const double speed = 1.0;
+	//const unsigned repeat_max = 100;
+
+	//double dt = 1.0;
+	//double time = 0.0;	// Elapsed time
+	//double dist = 0.0;	// Distance
+	//int i;
+
+	//for (unsigned i = 0; i < repeat_max; i += 1)
+	//{
+	//	dist += speed * dt; // dist = dist + speed * dt;
+	//	time += dt;
+	//	dt *= 0.5;
+	//	printf("Elapsed time = %.10fs, Distance = %.10fm\n", time, dist);
+	//}
+
+	//Note: Infinite geometric series
+
 	const double speed = 1.0;
-	const unsigned repeat_max = 100;
+	const unsigned repeat_max = 50;
 
-	double dt = 1.0;
-	double time = 0.0;	// Elapsed time
-	double dist = 0.0;
+	double time = 0.0;		// Elapsed time
+	double dist_arch = 0.0;	// Achilleus distance from start point
+	double dist_turtle = 1.0;	// rabbit distance from start point
+	double speed_arch = 10.0;
+	double speed_turtle = 0.001;
+	double dt = 0.01;
+	unsigned i;
 
-	for (unsigned i = 0; i < repeat_max; i++)
+	printf("Time = %fs, dt = %fs, Archilleus = %fm, turtle = %fm\n",
+		time, dt, dist_arch, dist_turtle);
+
+	for (i = 0; i < repeat_max; i++)
 	{
-		dist += speed * dt;
+		dist_arch = dist_arch + speed_arch * dt;
+		dist_turtle = dist_turtle + speed_turtle * dt;
 		time += dt;
-
-		printf("Elapsed time = %.10fs, Distance = %.10fm\n", dt, dist);
-		
-		dt /= 2.0; // dt = dt * 0.5;
+		dt *= 0.5;
+		printf("Time = %fs, dt = %fs, Archilleus = %fm, turtle = %fm\n",
+			time, dt, dist_arch, dist_turtle);
 	}
-
-	// by myself
-	/*
-	double speed = 1.0;
-	double dt;
-	double distance;
-	int iter;
-
-	for (dt = 1.0; dt <= 10.0; dt++)
-	{
-		distance = speed * dt;
-		printf("Elapsed time = %fs, Distance = %fm\n", dt, distance);
-	}
-	*/
-
-	/*
-		Zeno's Paradox
-
-		- Step 1 : 1.0 * 1.0 = 1.0, 1.0
-		- Step 2 : 1.0 * 0.5 = 0.5, 1.0 + 0.5 = 1.5
-		- Step 3 : 1.0 * 0.25 = 0.25, 1.5 + 0.25 = 1.75	
-		... ... 
-	*/
-
-	//Note: infinite geometric series
-
 	return 0;
 }
+
+
+//
+//const double speed = 1.0;
+//const unsigned repeat_max = 100;
+//
+//double dt = 1.0;
+//double time = 0.0;	// Elapsed time
+//double dist = 0.0;
+//
+//for (unsigned i = 0; i < repeat_max; i++)
+//{
+//	dist += speed * dt;
+//	time += dt;
+//
+//	printf("Elapsed time = %.10fs, Distance = %.10fm\n", dt, dist);
+//
+//	dt /= 2.0; // dt = dt * 0.5;
+//}
+
+// by myself
+/*
+double speed = 1.0;
+double dt;
+double distance;
+int iter;
+
+for (dt = 1.0; dt <= 10.0; dt++)
+{
+	distance = speed * dt;
+	printf("Elapsed time = %fs, Distance = %fm\n", dt, distance);
+}
+*/
+
+/*
+	Zeno's Paradox
+
+	- Step 1 : 1.0 * 1.0 = 1.0, 1.0
+	- Step 2 : 1.0 * 0.5 = 0.5, 1.0 + 0.5 = 1.5
+	- Step 3 : 1.0 * 0.25 = 0.25, 1.5 + 0.25 = 1.75
+	... ...
+*/
+
+//Note: infinite geometric series
