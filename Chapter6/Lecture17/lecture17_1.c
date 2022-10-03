e884770904b82995d8e169e9f81a50c45adbd35f
leeyunhome <leeyunhome@daum.net>
2022-10-03 21:49:12 +0900

    for loop를 여러개로 쪼개는 게 효율적일 수도 있다.

--- Chapter6/Lecture17/lecture17_1.c
+++ Chapter6/Lecture17/lecture17_1.c
@@ -5,31 +5,48 @@
 
 int main()
 {
-	/* Motivation */
-	//int i1 = 0;
-	//int i2 = 1;
-	//int i3 = 2;
-	//// ...
+	//int number_of_input = 0;
+	int input_num[SIZE] = { 0, };
+	int total_sum = 0;
+	int r;
+	int flag = 0;
+	//printf("Input number of input : ");
+	//int flag = scanf("%d", &number_of_input);
+	//printf("Enter %d numbers : ___\b\b\b", number_of_input);
+	printf("Enter 5 numbers : ___\b\b\b");
 
-	//printf("%d\n", i1);
-	//printf("%d\n", i2);
-	//printf("%d\n", i3);
-	//// ... 
-
-	int my_arr[SIZE];
-
-	// prepare for array data
-	for (int i = 0; i < SIZE; i++)
+	for (r = 0; r < SIZE; r++)
 	{
-		my_arr[i] = i * i;
-		1 + 2; // dummy
+		flag = scanf("%d", &input_num[r]);
+		total_sum += input_num[r];
 	}
 
-	// print array data
-	for (int i = 0; i < SIZE; i++)
-		printf("%d ", my_arr[i]);
+	printf("Sum = %d\n", total_sum);
 
-	//TODO: try debugger
+	///* Motivation */
+	////int i1 = 0;
+	////int i2 = 1;
+	////int i3 = 2;
+	////// ...
+
+	////printf("%d\n", i1);
+	////printf("%d\n", i2);
+	////printf("%d\n", i3);
+	////// ... 
+
+	//int my_arr[SIZE];
+
+	//// prepare for array data
+	//for (int i = 0; i < SIZE; i++)
+	//{
+	//	my_arr[i] = i * i;
+	//}
+
+	//// print array data
+	//for (int i = 0; i < SIZE; i++)
+	//	printf("%d ", my_arr[i]);
+
+	////TODO: try debugger
 
 	return 0;
 }
